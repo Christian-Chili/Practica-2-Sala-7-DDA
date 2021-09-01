5b1cc42f9eb3f86786300e82ea9aac148f41317a
christian Chili <christian.chili9@gmail.com>
2021-09-01 02:25:06 -0500

    Ejecicio propuesto: En este utilizamos todas las tecnicas aprendidas en la practica

---
+++ Artefactos/Artefactos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Artefactos.Global" Language="C#" %>
---
+++ Artefactos/Artefactos/Site.Master.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Artefactos
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Grafico.Visible = false;
+        }
+        protected void MostrarGrafico_ServerClick(Object sender, EventArgs e)
+        {
+            Grafico.Src = lista.Value + ".jpg";
+            Grafico.Alt = "Grafico Temperatura";
+            Grafico.Visible = true;
+        }
+    }
+}
